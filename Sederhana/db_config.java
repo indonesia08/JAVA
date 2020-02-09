b9f41cff88381a25bbccdd09bc96d60faa9497c0
Yusuf <40708511+indonesia08@users.noreply.github.com>
2020-02-09 10:59:54 +0700

    Create db_config.java

---
+++ Sederhana/db_config.java
@@ -0,0 +1,34 @@
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+//@author yusufhome
+public class db_config {
+    private static Connection connection;
+
+    public static Connection getConnection(){
+        //pengecekan koneksi database
+        if (connection==null){
+            try{
+               
+                String host     = "localhost";
+                String username = "root";
+                String password = "";
+                String db_name  = "test";
+                String port     = "3306";
+                
+                String url = "jdbc:mysql://"+host+":"+port+"/"+db_name;
+                
+                MysqlDataSource source = new MysqlDataSource();
+                source.setUser(username);
+                source.setPassword(password);
+                source.setURL(url);
+                connection = source.getConnection();
+                System.out.println("sukses menyambung ke database");
+            } catch (SQLException ex){
+                System.out.println("tidak dapat menyambung ke database");
+            }
+        }
+        return connection;
+    }
+}
